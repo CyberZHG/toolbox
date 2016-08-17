50d6c2fc1ae0bf4437d8e95cd644cfb29d543048
CyberZHG <CyberZHG@gmail.com>
2016-08-17 19:20:08 +0800

    Add random hex number.
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- build.py
+++ build.py
@@ -4,11 +4,11 @@ PARTS_FOLDER = 'parts'
 MAGIC_NUMBER = '75CAF6CF'
 
 with open('template.html') as reader:
-    html = reader.read()
+    template = reader.read()
 
 for file_name in os.listdir(PARTS_FOLDER):
     file_path = os.path.join(PARTS_FOLDER, file_name)
     with open(file_path) as reader:
-        html = html.replace(MAGIC_NUMBER, reader.read())
+        html = template.replace(MAGIC_NUMBER, reader.read())
     with open(file_name, 'w') as writer:
         writer.write(html)
