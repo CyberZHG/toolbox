c845339c062740bbd5d91543a12386e546c6fa18
CyberZHG <CyberZHG@gmail.com>
2016-08-17 17:10:28 +0800

    Build.
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

---
+++ build.py
@@ -0,0 +1,14 @@
+import os
+
+PARTS_FOLDER = 'parts'
+MAGIC_NUMBER = '75CAF6CF'
+
+with open('template.html') as reader:
+    html = reader.read()
+
+for file_name in os.listdir(PARTS_FOLDER):
+    file_path = os.path.join(PARTS_FOLDER, file_name)
+    with open(file_path) as reader:
+        html = html.replace(MAGIC_NUMBER, reader.read())
+    with open(file_name, 'w') as writer:
+        writer.write(html)
