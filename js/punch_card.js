167bf23c86e0e9cf12139125ce7d00e58301abe3
CyberZHG <CyberZHG@gmail.com>
2018-01-17 19:07:42 +0800

    Draw punch card

--- js/punch_card.js
+++ js/punch_card.js
@@ -4,6 +4,8 @@
 $(document).ready(function () {
     'use strict';
 
+    var buckets = [];
+
     function getLastYear() {
         return new Date(new Date().setFullYear(new Date().getFullYear() - 1));
     }
@@ -23,11 +25,13 @@ $(document).ready(function () {
     }
 
     function getBuckets(commits) {
-        var i, j, date, buckets = [];
-        for (i = 0; i < 7; i += 1) {
-            buckets.push([]);
-            for (j = 0; j < 24; j += 1) {
-                buckets[i].push(0);
+        var i, j, date;
+        if (buckets.length === 0) {
+            for (i = 0; i < 7; i += 1) {
+                buckets.push([]);
+                for (j = 0; j < 24; j += 1) {
+                    buckets[i].push(0);
+                }
             }
         }
         for (i = 0; i < commits.length; i += 1) {
@@ -37,6 +41,61 @@ $(document).ready(function () {
         return buckets;
     }
 
+    function drawPunchCard(buckets) {
+        var i, j,
+            maxCommitCnt = 0,
+            days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
+            canvas = $('#canvas_card'),
+            context = canvas[0].getContext('2d'),
+            width = canvas.width(),
+            height = canvas.height(),
+            widthInt = width / 26,
+            heightInt = height / 9;
+        for (i = 0; i < buckets.length; i += 1) {
+            for (j = 0; j < buckets[i].length; j += 1) {
+                if (buckets[i][j] > maxCommitCnt) {
+                    maxCommitCnt = buckets[i][j];
+                }
+            }
+        }
+        maxCommitCnt += 1e-6;
+        context.clearRect(0, 0, width, height);
+        context.fillStyle = 'black';
+        context.textAlign = 'center';
+        context.textBaseline = 'middle';
+        for (i = 0; i < days.length; i += 1) {
+            context.fillText(days[i], widthInt, heightInt * (i + 1));
+        }
+        for (j = 0; j < 24; j += 1) {
+            context.fillText(j, widthInt * (j + 2), heightInt * 8);
+        }
+        for (i = 0; i < buckets.length; i += 1) {
+            for (j = 0; j < buckets[i].length; j += 1) {
+                if (buckets[i][j] !== 0) {
+                    context.beginPath();
+                    context.arc(
+                        widthInt * (j + 2),
+                        heightInt * (i + 1),
+                        Math.min(widthInt, heightInt) * 0.45 * Math.sqrt(buckets[i][j] / maxCommitCnt),
+                        0,
+                        2 * Math.PI
+                    );
+                    context.fillStyle = 'rgba(0, 0, 0, ' + (0.4 + 0.6 * buckets[i][j] / maxCommitCnt) + ')';
+                    context.fill();
+                }
+            }
+        }
+    }
+
+    function queryCommits(url, page) {
+        $.getJSON(url + '&page=' + page, function (commits) {
+            if (commits.length > 0) {
+                drawPunchCard(getBuckets(commits));
+                queryCommits(url, page + 1);
+            }
+        });
+    }
+
     $('#button_gen').click(function () {
         var url = 'https://api.github.com/repos/',
             owner = $('#owner').val(),
@@ -44,7 +103,12 @@ $(document).ready(function () {
             author = $('#author').val(),
             prev = getISOString(getLastYear()),
             now = getISOString(new Date()),
+            canvas = $('#canvas_card'),
+            width = canvas.parent().width(),
+            height = canvas.parent().height() * 0.6,
             params = {};
+        canvas.attr('width', width);
+        canvas.attr('height', height);
         url += owner + '/' + repo + '/commits';
         params.since = prev;
         params.until = now;
@@ -52,6 +116,29 @@ $(document).ready(function () {
             params.author = author;
         }
         url += '?' + objToQuery(params);
+        queryCommits(url, 1);
     });
+
+    function getParameterByName(name) {
+        var url = window.location.href,
+            regex,
+            results;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+        results = regex.exec(url);
+        if (!results) {
+            return null;
+        }
+        if (!results[2]) {
+            return '';
+        }
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    if (getParameterByName('owner') && getParameterByName('repo')) {
+        $('#owner').val(getParameterByName('owner'));
+        $('#repo').val(getParameterByName('repo'));
+        $('#button_gen').click();
+    }
 
 });
