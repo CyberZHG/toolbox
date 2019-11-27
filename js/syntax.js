e1c2e2623aa3a4f1c6be81ad898d794f8d3400a9
KimbingNg <835598686@qq.com>
2019-11-27 19:05:26 +0800

    fix bug on function calcLR1Closure

--- js/syntax.js
+++ js/syntax.js
@@ -648,9 +648,9 @@ function calcLR1Closure(grammar, items, preNullables, preFirsts) {
                                 items.push(item);
                                 hasNewItem = true;
                             } else {
-                                for (m = 0; m < items[i].lookahead; m += 1) {
-                                    if (closure.nonkernel[index].lookahead.indexOf(items[i].lookahead[m]) < 0) {
-                                        closure.nonkernel[index].lookahead.push(items[i].lookahead[m]);
+                                for(m = 0; m < item.lookahead.length; m += 1){
+                                    if(closure.nonkernel[index].lookahead.indexOf(item.lookahead[m]) < 0){
+                                        closure.nonkernel[index].lookahead.push(item.lookahead[m]);
                                         hasNewItem = true;
                                     }
                                 }
