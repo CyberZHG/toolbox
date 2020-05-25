4b98cfeca51dfc4f17697da52a0e42c3acb424b8
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2020-05-26 00:12:14 +0800

    Remove unreachable terms in CNF

--- js/cfg2cnf.js
+++ js/cfg2cnf.js
@@ -191,6 +191,32 @@ $(document).ready(function () {
         return grammar;
     }
 
+    function removeUnreachable(grammar) {
+        var i, j, k, front = 0, term,
+            keys = Object.keys(grammar),
+            queue = [keys[0]],
+            reachable = {};
+        reachable[keys[0]] = true;
+        while (front < queue.length) {
+            for (i = 0; i < grammar[queue[front]].length; i += 1) {
+                for (j = 0; j < grammar[queue[front]][i].length; j += 1) {
+                    term = grammar[queue[front]][i][j];
+                    if (grammar.hasOwnProperty(term) && !reachable.hasOwnProperty(term)) {
+                        queue.push(term);
+                        reachable[term] = true;
+                    }
+                }
+            }
+            front += 1;
+        }
+        for (i = 0; i < keys.length; i += 1) {
+            if (!reachable.hasOwnProperty(keys[i])) {
+                delete grammar[keys[i]];
+            }
+        }
+        return grammar;
+    }
+
     function getParameterByName(name) {
         var url = window.location.href,
             regex,
@@ -212,7 +238,8 @@ $(document).ready(function () {
             emptied = removeEmpties(grammar),
             singled = removeSingles(emptied),
             converted = converGrammar(singled),
-            pretty = toPretty(converted),
+            linked = removeUnreachable(converted),
+            pretty = toPretty(linked),
             prefix = window.location.href.split('?')[0] + '?grammar=',
             input = b64EncodeUnicode($('#input_grammar').val());
         $('#input_url').val(prefix + input);
